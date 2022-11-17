d7405f5981ce43573f9c9f85db70e5e669b369e3
Zarina-2003 <107272179+Zarina-2003@users.noreply.github.com>
2022-11-17 17:24:09 +0500

    Add files via upload

---
+++ 41/script.js
@@ -0,0 +1,181 @@
+let arr = [
+    {
+        id: "1",
+        name: "MYLLRA",
+        subtitle: "Crib with drawer, white, ",
+        price: 449.99,
+        rating: 5,
+        delivery: true,
+        description: "Your child can create their own little world under this tent. A calm nook where they can feel cosy, read and play without being disturbed – and fall asleep snug. The tough dinosaurs protect against both rowdy siblings and scary dreams.",
+        media: ["https://www.ikea.com/us/en/images/products/myllra-crib-with-drawer-white__0812403_pe783210_s5.jpg?f=xl", "https://www.ikea.com/us/en/images/products/myllra-crib-with-drawer-white__0812402_pe783212_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/myllra-crib-with-drawer-white__0812401_pe783213_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/myllra-crib-with-drawer-white__0947574_pe798596_s5.jpg?f=s",],
+        type: "Children's room",
+        size: " 27 1/2x52",
+        isRecycling: false
+    },
+    {
+        id: "2",
+        name: "TROFAST",
+        subtitle: "Storage combination, white/gray ",
+        price: 99.99,
+        rating: 4.5,
+        delivery: true,
+        description: "Your child can create their own little world under this tent. A calm nook where they can feel cosy, read and play without being disturbed – and fall asleep snug. The tough dinosaurs protect against both rowdy siblings and scary dreams.",
+        media: ["https://www.ikea.com/us/en/images/products/trofast-storage-combination-white-gray__0807589_pe770448_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/trofast-storage-combination-white-gray__1046550_pe843027_s5.jpg?f=xl", "https://www.ikea.com/us/en/images/products/trofast-storage-combination-white-gray__1046551_pe843026_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/trofast-storage-combination-white-orange__0807680_pe770479_s5.jpg?f=s",],
+        type: "Children's room",
+        size: "39x17 3/8x37",
+        isRecycling: false
+    },
+    {
+        id: "3",
+        name: "ANTILOP",
+        subtitle: "High chair with safety belt, white/silver color",
+        price: 21.99,
+        rating: 4.5,
+        delivery: true,
+        description: "Your child can create their own little world under this tent. A calm nook where they can feel cosy, read and play without being disturbed – and fall asleep snug. The tough dinosaurs protect against both rowdy siblings and scary dreams.",
+        media: ["https://www.ikea.com/us/en/images/products/antilop-high-chair-with-safety-belt-white-silver-color__0727491_pe735716_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/antilop-high-chair-with-safety-belt-white-silver-color__0873648_pe613162_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/antilop-high-chair-with-safety-belt-white-silver-color__0873655_pe613163_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/antilop-high-chair-with-safety-belt-white-silver-color__0873777_pe613165_s5.jpg?f=s",],
+        type: "Children's room",
+        size: "0",
+        isRecycling: false
+    },
+    {
+        id: "4",
+        name: "SEKTION / MAXIMERA",
+        subtitle: "Base cabinet with 3 drawers, white Enköping/white wood effect",
+        price: 314.00,
+        rating: 4.5,
+        delivery: true,
+        description: "Your child can create their own little world under this tent. A calm nook where they can feel cosy, read and play without being disturbed – and fall asleep snug. The tough dinosaurs protect against both rowdy siblings and scary dreams.",
+        media: ["https://www.ikea.com/us/en/images/products/sektion-maximera-base-cabinet-with-3-drawers-white-bodarp-gray-green__0769954_pe755356_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/sektion-maximera-base-cabinet-with-3-drawers-white-bodarp-gray-green__0766781_pe753897_s5.jpg?f=xl", "https://www.ikea.com/us/en/images/products/sektion-maximera-base-cabinet-with-3-drawers-white-bodarp-gray-green__1030310_ph165000_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/sektion-maximera-base-cabinet-with-3-drawers-white-bodarp-gray-green__1011630_ph174301_s5.jpg?f=s",],
+        type: "Kitchen",
+        size: "  24x24x30",
+        isRecycling: false
+    },
+    {
+        id: "5",
+        name: "VADHOLMA",
+        subtitle: "Open storage, brown/stained ash",
+        price: 81.00,
+        rating: 4,
+        delivery: true,
+        description: "Your child can create their own little world under this tent. A calm nook where they can feel cosy, read and play without being disturbed – and fall asleep snug. The tough dinosaurs protect against both rowdy siblings and scary dreams.",
+        media: ["https://www.ikea.com/us/en/images/products/vadholma-open-storage-brown-stained-ash__0551843_pe658811_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/vadholma-open-storage-brown-stained-ash__0862710_pe689372_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/vadholma-open-storage-brown-stained-ash__0862718_pe692042_s5.jpg?f=s"],
+        type: "Kitchen",
+        size: "9x14 3/8x30",
+        isRecycling: false
+    },
+    {
+        id: "6",
+        name: "KILSVIKEN",
+        subtitle: "Sink, black quartz composite",
+        price: 227.00,
+        rating: 5,
+        delivery: true,
+        description: "Your child can create their own little world under this tent. A calm nook where they can feel cosy, read and play without being disturbed – and fall asleep snug. The tough dinosaurs protect against both rowdy siblings and scary dreams.",
+        media: ["https://www.ikea.com/us/en/images/products/kilsviken-sink-black-quartz-composite__0831716_pe777316_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/kilsviken-sink-black-quartz-composite__0916540_pe785240_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/kilsviken-sink-black-quartz-composite__0916701_pe785346_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/kilsviken-sink-black-quartz-composite__1034191_ph176366_s5.jpg?f=s",],
+        type: "Kitchen",
+        size: "22x18 1/8",
+        isRecycling: false
+    },
+    {
+        id: "7",
+        name: "CIRKULERA",
+        subtitle: "Range with glass ceramic cooktop, Stainless steel",
+        price: 729.00,
+        rating: 5,
+        delivery: true,
+        description: "Your child can create their own little world under this tent. A calm nook where they can feel cosy, read and play without being disturbed – and fall asleep snug. The tough dinosaurs protect against both rowdy siblings and scary dreams.",
+        media: ["https://www.ikea.com/us/en/images/products/cirkulera-range-with-glass-ceramic-cooktop-stainless-steel__1065954_pe852250_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/cirkulera-range-with-glass-ceramic-cooktop-stainless-steel__1065950_pe852248_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/cirkulera-range-with-glass-ceramic-cooktop-stainless-steel__1065953_pe852251_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/cirkulera-range-with-glass-ceramic-cooktop-stainless-steel__1065952_pe852252_s5.jpg?f=s",],
+        type: "Kitchen",
+        size: "0",
+        isRecycling: false
+    },
+    {
+        id: "8",
+        name: "ENHET",
+        subtitle: "Bathroom, anthracite/gray frame",
+        price: 752.56,
+        rating: 5,
+        delivery: true,
+        description: "Your child can create their own little world under this tent. A calm nook where they can feel cosy, read and play without being disturbed – and fall asleep snug. The tough dinosaurs protect against both rowdy siblings and scary dreams.",
+        media: ["https://www.ikea.com/us/en/images/products/enhet-bathroom-anthracite-gray-frame__1051969_pe845856_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/enhet-bathroom-anthracite-gray-frame__1055419_pe847990_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/enhet-bathroom-anthracite-gray-frame__1049543_pe844243_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/enhet-bathroom-anthracite-gray-frame__1051916_pe845809_s5.jpg?f=s",],
+        type: "Bathroom",
+        size: "18x18x24",
+        isRecycling: false
+    },
+    {
+        id: "9",
+        name: "NORRMANSÖ / NORRMANSÖ",
+        subtitle: "Table+6 chairs, outdoor, acacia/beige acacia",
+        price: 1.039,
+        rating: 5,
+        delivery: true,
+        description: "Your child can create their own little world under this tent. A calm nook where they can feel cosy, read and play without being disturbed – and fall asleep snug. The tough dinosaurs protect against both rowdy siblings and scary dreams.",
+        media: ["https://www.ikea.com/us/en/images/products/norrmansoe-norrmansoe-table-6-chairs-outdoor-acacia-beige-acacia__1078032_pe857068_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/norrmansoe-norrmansoe-table-6-chairs-outdoor-acacia-beige-acacia__1078033_pe857070_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/norrmansoe-norrmansoe-table-6-chairs-outdoor-acacia-beige-acacia__1078031_pe857069_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/norrmansoe-norrmansoe-table-6-chairs-outdoor-acacia-beige-acacia__1077693_pe856978_s5.jpg?f=s",],
+        type: "Outdoor",
+        size: "86 5/8x39 3/8 ",
+        isRecycling: false
+    },
+    {
+        id: "10",
+        name: "SOLLERÖN",
+        subtitle: "3-seat modular sofa, outdoor, brown/Frösön/Duvholmen dark gray",
+        price: 449.99,
+        rating: 5,
+        delivery: true,
+        description: "Your child can create their own little world under this tent. A calm nook where they can feel cosy, read and play without being disturbed – and fall asleep snug. The tough dinosaurs protect against both rowdy siblings and scary dreams.",
+        media: ["https://www.ikea.com/us/en/images/products/solleroen-3-seat-modular-sofa-outdoor-brown-froesoen-duvholmen-dark-gray__0589835_pe673506_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/solleroen-3-seat-modular-sofa-outdoor-brown-froesoen-duvholmen-dark-gray__0684265_pe721085_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/solleroen-3-seat-modular-sofa-outdoor-brown-froesoen-duvholmen-dark-gray__0673314_pe717178_s5.jpg?f=s", "https://www.ikea.com/us/en/images/products/solleroen-3-seat-modular-sofa-outdoor-brown-froesoen-duvholmen-dark-gray__0733031_pe738830_s5.jpg?f=s",],
+        type: "Children's room",
+        size: "87 3/4x32 1/4x34 5/8",
+        isRecycling: false
+    },
+]
+
+let main = document.querySelector('.main')
+
+function reload(arr) {
+    main.innerHTML = ''
+    for (let item of arr) {
+        let box = document.createElement('div')
+        let imgs = document.createElement('div')
+        let img1 = document.createElement('img')
+        let img2 = document.createElement('img')
+        let img3 = document.createElement('img')
+        let img4 = document.createElement('img')
+        let name = document.createElement('p')
+        let subtitle = document.createElement('p')
+        let price = document.createElement('p')
+        let rating = document.createElement('p')
+        let delivery = document.createElement('p')
+        let type = document.createElement('p')
+        let size = document.createElement('p')
+        let isRecycling = document.createElement('p')
+
+
+        box.classList.add('box')
+        imgs.classList.add('imgs')
+
+        img1.src = item.media[0]
+        img2.src = item.media[1]
+        img3.src = item.media[2]
+        img4.src = item.media[3]
+
+        name.innerHTML = 'name ' + item.name
+        subtitle.innerHTML = 'subtitle ' + item.subtitle
+        price.innerHTML = 'price ' + item.price
+        rating.innerHTML = 'rating ' + item.rating
+        delivery.innerHTML = 'delivery ' + item.delivery
+        type.innerHTML = 'type ' + item.type
+        size.innerHTML = 'size ' + item.size
+        isRecycling.innerHTML = 'isRecycling ' + item.isRecycling
+
+
+
+        main.append(box)
+        box.append( name, subtitle, price, rating, delivery, type, size, isRecycling, imgs)
+        imgs.append(img1 ,img2,img3,img4)
+    
+        
+    }
+}
+reload(arr)
